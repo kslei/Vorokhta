fd269a3ee3c669611fd97a4e2516d28b09eae9c7
Sergey <72152127+kslei@users.noreply.github.com>
2023-02-17 18:59:38 +0200

    Second

--- js/book.js
+++ js/book.js
@@ -19,28 +19,40 @@ function createpage(num, direction) {
   if (num%2 === 0) {
     pagesItemsL.appendChild(page);
     page.style.zIndex = num;
-    direction? page.style.width = "0%" : page.style.width = "100%";
+    
     page.style.right = 0;
-    page.style.boxShadow = "-3px 3px 5px black";
+    if (window.innerWidth >= 768) {
+      page.style.boxShadow = "-3px 3px 5px black";
+      direction? page.style.width = "0%" : page.style.width = "100%";
+      page.style.height = "100%";
+    } else {
+      page.style.boxShadow = "3px -3px 5px black";
+      direction? page.style.height = "0%" : page.style.height = "100%";
+      page.style.width = "100%";
+    }
   } else {
     pagesItemsR.appendChild(page);
     page.style.zIndex = max - num;
-    direction? page.style.width = "100%" : page.style.width = "0%";
+    if (window.innerWidth >= 768) {
+      direction? page.style.width = "100%" : page.style.width = "0%";
+      page.style.height = "100%";
+    } else  {
+      direction? page.style.height = "100%" : page.style.height = "0%";
+      page.style.width = "100%";
+    }
+    
     page.style.left = 0;
     page.style.boxShadow = "3px 3px 5px black";
   }
   if(num === 0) {page.style.opacity = 0} else {page.style.opacity = 1}
-  let image = document.createElement('div');
-  image.className = 'pages__image';
-  page.appendChild(image);
   let img = document.createElement('img');
   if (num !== 0) img.src = './img/' + num + '.jpg';
-  image.appendChild(img);
+  page.appendChild(img);
 }
 
 function playAudio() {
-  var myAudio = new Audio;
-  myAudio.src = "../sounds/book.wav";
+  var myAudio = new Audio("./sounds/book.mp3");
+  myAudio.autoplay = true;
   myAudio.play();
 }
 
@@ -48,24 +60,40 @@ forv.addEventListener('click', function() {
   if (indexPage +1 <= max) {
     indexPage += 2;
     pages = [indexPage-1, indexPage];
-    console.log(indexPage)
     if (indexPage+1 > max) {forv.style.opacity = 0} 
     if (indexPage > 1) { back.style.opacity = 1 }
     console.log(pages)
     render(true)
     playAudio()
-    pagesItemsR.lastChild.previousElementSibling.style.width = "100%";
-    pagesItemsR.lastChild.previousElementSibling.style.transition = "400ms";
-    pagesItemsR.lastChild.previousElementSibling.style.width = 0;
-    pagesItemsL.lastChild.style.width = 0;
-    pagesItemsL.lastChild.style.transition = "400ms";
-    setTimeout(()=>{
-      pagesItemsL.lastChild.style.width = "100%";
-      pagesItemsR.removeChild(pagesItemsR.lastChild.previousElementSibling);
+    if (window.innerWidth >= 768) {
+      pagesItemsR.lastChild.previousElementSibling.style.width = "100%";
+      pagesItemsR.lastChild.previousElementSibling.style.transition = "400ms";
+      pagesItemsR.lastChild.previousElementSibling.style.width = 0;
+      pagesItemsL.lastChild.style.width = 0;
+      pagesItemsL.lastChild.style.transition = "400ms";
       setTimeout(()=>{
-        pagesItemsL.removeChild(pagesItemsL.lastChild.previousElementSibling);
+        pagesItemsL.lastChild.style.width = "100%";
+        pagesItemsR.removeChild(pagesItemsR.lastChild.previousElementSibling);
+        setTimeout(()=>{
+          pagesItemsL.removeChild(pagesItemsL.lastChild.previousElementSibling);
+        }, 400)
       }, 400)
-    }, 400)
+    } else {
+      pagesItemsR.lastChild.previousElementSibling.style.height = "100%";
+      pagesItemsR.lastChild.previousElementSibling.style.transition = "400ms";
+      pagesItemsR.lastChild.previousElementSibling.style.height = 0;
+      pagesItemsL.lastChild.style.height = 0;
+      pagesItemsL.lastChild.style.top = "100%";
+      pagesItemsL.lastChild.style.transition = "400ms";
+      setTimeout(()=>{
+        pagesItemsL.lastChild.style.height = "100%";
+        pagesItemsL.lastChild.style.top = 0;
+        pagesItemsR.removeChild(pagesItemsR.lastChild.previousElementSibling);
+        setTimeout(()=>{
+          pagesItemsL.removeChild(pagesItemsL.lastChild.previousElementSibling);
+        }, 400)
+      }, 400)
+    }
   }  
 })
 
@@ -78,20 +106,36 @@ back.addEventListener('click', function () {
     pages = [indexPage - 1, indexPage];
     render(false)
     playAudio()
-    pagesItemsL.lastChild.previousElementSibling.style.width = "100%";
-    pagesItemsL.lastChild.previousElementSibling.style.transition = "400ms";
-    pagesItemsL.lastChild.previousElementSibling.style.width = 0;
-    console.log(pagesItemsL.lastChild)
-    pagesItemsR.lastChild.style.width = 0;
-    pagesItemsR.lastChild.style.transition = "400ms";
-    setTimeout(() => {
-      pagesItemsR.lastChild.style.width = "100%";
-      pagesItemsL.removeChild(pagesItemsL.lastChild.previousElementSibling);
+    if (window.innerWidth >= 768) {
+      pagesItemsL.lastChild.previousElementSibling.style.width = "100%";
+      pagesItemsL.lastChild.previousElementSibling.style.transition = "400ms";
+      pagesItemsL.lastChild.previousElementSibling.style.width = 0;
+      pagesItemsR.lastChild.style.width = 0;
+      pagesItemsR.lastChild.style.transition = "400ms";
       setTimeout(() => {
-        pagesItemsR.removeChild(pagesItemsR.lastChild.previousElementSibling);
+        pagesItemsR.lastChild.style.width = "100%";
+        pagesItemsL.removeChild(pagesItemsL.lastChild.previousElementSibling);
+        setTimeout(() => {
+          pagesItemsR.removeChild(pagesItemsR.lastChild.previousElementSibling);
+        }, 400)
       }, 400)
-    }, 400)
-}
+    } else {
+      pagesItemsL.lastChild.previousElementSibling.style.height = "100%";
+      pagesItemsL.lastChild.previousElementSibling.style.top = 0;
+      pagesItemsL.lastChild.previousElementSibling.style.transition = "400ms";
+      pagesItemsL.lastChild.previousElementSibling.style.top = "100%";
+      pagesItemsL.lastChild.previousElementSibling.style.height = 0;
+      pagesItemsR.lastChild.style.height = 0;
+      pagesItemsR.lastChild.style.transition = "400ms";
+      setTimeout(() => {
+        pagesItemsR.lastChild.style.height = "100%";
+        pagesItemsL.removeChild(pagesItemsL.lastChild.previousElementSibling);
+        setTimeout(() => {
+          pagesItemsR.removeChild(pagesItemsR.lastChild.previousElementSibling);
+        }, 400)
+      }, 400)
+    }
+  }
 })
 
 render(true)
